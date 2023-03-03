e2b7c28ee066e2ae649bc1a332544b0ae02388e1
s21dororebe <s21dororebe@venta.lv>
2023-03-03 14:34:36 +0200

    Edit: setters & getters in Student class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,51 @@
 package model;
 
 public class Student {
+    //1. variables
+    private long id;
+    private String name, lastName;
+    private Faculty faculty;
+    private String personCode;
 
+    //2. getters and setters
+    public long getId(){
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    //ToDo take a look regex101.com
+    /// [A-ZĀĒŖŪĪĻĶĢŠŽČŅ]{1}[a-zēŗūīļķģšāžčņ]+[ ]?([A-ZĀĒŖŪĪĻĶĢŠŽČŅ]{1}[a-zēŗūīļķģšāžčņ]+)?
+    public void setName(String inputName) {
+        if (inputName.matches("[A-ZĀĒŖŪĪĻĶĢŠŽČŅ]{1}[a-zēŗūīļķģšāžčņ]+[ ]?([A-ZĀĒŖŪĪĻĶĢŠŽČŅ]{1}[a-zēŗūīļķģšāžčņ]+)?") ){
+            name = inputName;
+        } else {
+            name = "Unknown";
+        }
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public Faculty getFaculty() {
+        return faculty;
+    }
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
+    public String getPersonCode() {
+        return personCode;
+    }
+    public void setPersonCode(String personCode) {
+        this.personCode = personCode;
+    }
+
+    //3. constructors
+    //4. toString
+    //5. additional functions
 }
